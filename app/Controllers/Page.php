e4bdcd0df99d71f012f2fd7d69d2bd3461a9eb4f
tengkureinaldo <56400536+tengkureinaldo@users.noreply.github.com>
2023-10-27 10:00:29 +0700

    Add files via upload

---
+++ app/Controllers/Page.php
@@ -0,0 +1,30 @@
+<?php namespace App\Controllers;
+
+class Page extends BaseController
+{
+	public function about()
+	{
+		echo "about page";
+	}
+    
+    public function contact()
+	{
+		echo "contact page";
+	}
+    
+    public function faqs()
+	{
+		echo "Faqs page";
+	}
+
+	public function tos()
+    {
+        echo "Halaman Term of Service";
+    }
+
+	public function sapaan()
+	{
+		echo "Hello World!!";
+	}
+
+}
